dfe672a0c90180d74c7e94387a9e03343eb411f2
MinhPhat <phatt@sfu.ca>
2023-11-19 00:30:29 -0800

    completed transmitter side

--- Digital_design/UART_Altera_DE1_Board/TX.vhdl
+++ Digital_design/UART_Altera_DE1_Board/TX.vhdl
@@ -12,7 +12,7 @@ TX_LINE: OUT STD_LOGIC
 );
 end TX;
 
-architecture main of TX is
+architecture behavior of TX is
 signal PRSCL: integer range 0 to 5208:=0;
 signal INDEX: integer range 0 to 9:=0;
 signal DATAFLL: std_logic_vector(9 downto 0);
@@ -21,26 +21,33 @@ begin
 process(CLK)
 begin
 if(CLK'event AND CLK='1') then
-	if (TX_FLG='0' and START='1') then
-		TX_FLG<='1';
-		BUSY<='1';
-		DATAFLL(0)<='0';
-		DATAFLL(9)<='1';
-		DATAFLL(8 downto 1)<=DATA;
+	if (TX_FLG = '0' and START = '1') then
+		TX_FLG <= '1';
+		BUSY <='1';
+		DATAFLL(0) <= '0';
+		DATAFLL(9) <= '1';
+		DATAFLL(8 downto 1) <= DATA;
 	end if;
 	
 	if(TX_FLG='1') then
-		if
-	
-	
-	
+		if(PRSCL<5207) then
+			PRSCL <= PRSCL + 1;
+		else 
+			PRSCL <= 0;
+		end if;
+		
+		if(PRSCL=2607) then
+			TX_LINE <= DATAFILL(INDEX);
+			if (INDEX < 9) then
+				INDEX <= INDEX + 1;
+			else
+				TX_FLG <= '0';
+				BUSY <= '0';
+			end if;
+		end if;
 	end if;
-
-
-
-
-
-
 end if;
+end process;
+end behavior;
 
 
