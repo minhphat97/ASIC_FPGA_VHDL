56509fe4817fe1e19400de941afb657ab04e2b7c
MinhPhat <phatt@sfu.ca>
2023-11-28 00:20:18 -0800

    creating RX entity part 2

--- Digital_design/UART_Altera_DE1_Board/RX.vhdl
+++ Digital_design/UART_Altera_DE1_Board/RX.vhdl
@@ -13,3 +13,31 @@ end RX;
 
 architecture nehavior of RX is
 signal DATAFLL: std_logic_vector(9 downto 0);
+signal RX_FLG: std_logic:='0';
+signal PRSCL: integer range 0 to 5208:=0;
+signal INDEX: integer range 0 to 9:=0;
+begin
+process(CLK)
+begin
+if (CLK'event and CLK='1')then
+	if (RX_FLG = '0' and RX_LINE = '0') then
+		INDEX <= 0;
+		PRSCL <= 0;
+		BUSY <= '1';
+		RX_FLG <= '1';
+	end if;
+	
+	if (RX_FLG = '1') then
+		DATAFILL(INDEX) <= RX_LINE;
+	
+	end if;
+
+
+
+
+
+
+
+end if;
+end process;
+end behavior;
